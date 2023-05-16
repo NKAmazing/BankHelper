26523b521db9f7b1bdcdeb36721c5336dd15ebe9
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-15 23:50:32 -0300

    routing and consumers technically working

--- server/consumers.py
+++ server/consumers.py
@@ -2,28 +2,18 @@ from channels.generic.websocket import WebsocketConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-# class MyConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.accept()
-
-#     def disconnect(self, close_code):
-#         pass
-
-#     def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-
-#         self.send(text_data=json.dumps({
-#             'message': message
-#         }))
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    
+    # Connects to the websocket
     async def connect(self):
         await self.accept()
 
+    # Disconnects from the websocket
     async def disconnect(self, close_code):
         pass
 
+    # Receives a message from the websocket
     async def receive(self, text_data):
         # Convertir los datos a formato JSON
         data = {
--- server/routing.py
+++ server/routing.py
@@ -2,10 +2,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from . import consumers
 
+# Establishes the websocket URL router
 websocket_urlpatterns = [
     path('ws/', consumers.MyConsumer.as_asgi()),
 ]
 
+# Establishes the protocol type router
 application = ProtocolTypeRouter({
     'websocket': URLRouter(
         websocket_urlpatterns
