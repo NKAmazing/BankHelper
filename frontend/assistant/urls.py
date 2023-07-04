72c98d13b95547154fc20d3c12b629e45ccb6b2d
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-07-04 02:08:52 -0300

    #15 staticfiles folder deleted

--- frontend/assistant/urls.py
+++ frontend/assistant/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('test', views.test, name='test'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('lobby/', views.lobby, name='lobby'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frontend/staticfiles/scripts/serverManager.js
+++
@@ -1,9 +0,0 @@
-function handleServerResponse(data) {
-    if (data.action === 'get_account_info') {
-        document.getElementById('result-container').textContent = data.result;
-    } else if (data.action === 'make_transaction') {
-        document.getElementById('result-container').textContent = data.result;
-    } else if (data.action === 'show_transactions_list') {
-        document.getElementById('result-container').textContent = data.result;
-    }
-  }
