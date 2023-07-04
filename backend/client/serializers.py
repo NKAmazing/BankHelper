625b80d4ebd63d3c5d31a4ab7c5288d1260c7f24
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-07-04 00:05:59 -0300

    #15 templates and socket configurations moved, WS doesn't work

--- backend/client/serializers.py
+++ backend/client/serializers.py
@@ -6,7 +6,3 @@ class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
-
-    # # this is the method responsible for insertion of data with hashed password
-    # def create(self, validated_data):
-    #     return User.objects.create_user(**validated_data)
--- backend/client/urls.py
+++ backend/client/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from client.controllers import user_controller as user
-from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('lobby/', views.lobby, name='lobby'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
     path('create_user/', user.create, name='create_user'),
     path('get_users/', user.get_all, name='get_all_users'),
     path('get_user/<int:id>/', user.get_by_id, name='get_user'),
